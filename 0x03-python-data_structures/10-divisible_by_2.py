efb4ff7a05f5e591450c6b47f63ea3b65356afbb
Mujor Bright <mujorb@gmail.com>
2022-06-06 15:58:07 -0700

    Fix for task ten

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,6 +6,6 @@ def divisible_by_2(my_list=[]):
         if my_list[i] % 2 == 0:
             check_div.append(True)
         else:
-           check_div.append(False)
+            check_div.append(False)
 
     return (check_div)
