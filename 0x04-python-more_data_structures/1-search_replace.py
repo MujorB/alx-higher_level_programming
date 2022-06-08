3b8ea5f7b7922d87ea7aa270c5c4ea71e8d92083
Mujor Bright <mujorb@gmail.com>
2022-06-08 10:01:50 -0700

    Solution to task One

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+def search_replace(my_list, search, replace):
+    
+    newlist = my_list[:]
+    list_len = len(my_list)
+    
+    for i in range(list_len):
+        if newlist[i] == search:
+            newlist[i] = replace
+    return newlist
