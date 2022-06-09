c10056ef2a9b5bb4878414375c6acd4cc7ea87d5
Mujor Bright <mujorb@gmail.com>
2022-06-08 20:45:24 -0700

    Solution to task Twelve

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    if roman_string == 0 or type(roman_string) != str:
+        return None
+
+    val = 0
+    sl = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num_l = len(roman_string)
+    for i in range(len(roman_string)):
+        if i != num_l - 1 and (sl[roman_string[i + 1]] > sl[roman_string[i]]):
+            val -= sl[roman_string[i]]
+        else:
+            val += sl[roman_string[i]]
+    return val
